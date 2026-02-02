c25dfa4cbcb40c8563c569116330424688310cfe
misbashaikh2004 <misba6602@gmail.com>
2026-02-02 18:52:06 +0530

    CORSSSS

--- CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
+++ CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
@@ -25,7 +25,7 @@ public class CustomerController {
 	@Autowired
 	private CustomerService cs;
 	
-	//aefwerf
+	//aefwerf efwefwefe
 	@GetMapping("/")
 	public String getMethodName(){
 		return "Hii";
